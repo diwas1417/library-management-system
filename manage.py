55e0ec6b855e3e81abc2446927beea9888d0dcdd
Ricky <diwasbasyal1417@gmail.com>
2025-05-28 22:38:54 +1000

    Refactored manage.py to fix Embold issues

--- manage.py
+++ manage.py
@@ -2,23 +2,24 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line, CommandError
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_project.settings")
     try:
-        from django.core.management import execute_from_command_line
+        execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    try:
-        execute_from_command_line(sys.argv)
-    except Exception as e:
-        print(f"An error occurred while executing command line: {e}")
+    # try:
+    #     execute_from_command_line(sys.argv)
+    # except CommandError as e:
+    #     print(f"Django command error: {e}")
 
 
 if __name__ == "__main__":
